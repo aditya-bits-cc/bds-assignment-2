17fe52e41d96fa134c5199d3573c15f327ac35ab
adityarj-pazuzu <aditya.jambhalikar001@gmail.com>
2025-04-26 19:04:47 +0530

    code reformatting

--- mongo_consistency_check.py
+++ mongo_consistency_check.py
@@ -18,35 +18,47 @@ MONGO_URI = f"mongodb+srv://{username}:{password}@{cluster}/?retryWrites=true&w=
 
 # Connect to MongoDB
 client = MongoClient(MONGO_URI)
-db = client.get_database("test_db1")
-collection = db.get_collection("test_db1")
 
-# Clear collection for clean testing (Optional for huge datasets, remove for bulk operations)
-collection.delete_many({})
-
-# Simulating a large dataset insert operation
+# Simulating a dataset insert operation
 large_dataset = [{"name": f"User {i}", "age": i % 1000} for i in range(1000)]
 
-def write_data(w_val=1):
-    start_time = time.time()
-    collection.with_options(write_concern=WriteConcern(w=w_val)).insert_many(large_dataset)
-    end_time = time.time()
-    print(f"Inserting large dataset with Write Concern {w_val}... Operation completed in {end_time - start_time:.4f} seconds.")
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"Operation `{func.__name__}` completed in {end_time - start_time:.4f} seconds")
+        return result
+    return wrapper
 
+@timer
+def write_data(w_val=1):
+    print(f"\nInserting large dataset with Write Concern {w_val}...", end=" ")
+    collection.with_options(write_concern=WriteConcern(w=w_val)).insert_many(large_dataset)
+    return None
+
+@timer
 def read_data(read_level="local"):
-    start_time = time.time()
-    document = collection.with_options(read_concern=ReadConcern(level=read_level)).find_one({"name": "User 50"})
-    end_time = time.time()
-    print(f"Reading with Read Concern '{read_level}'... Operation completed in {end_time - start_time:.4f} seconds.")
+    print(f"Reading with Read Concern '{read_level}'...", end=" ")
+    collection.with_options(read_concern=ReadConcern(level=read_level)).find_one({"name": "User 50"})
+    return None
+
+
+# DB and collection for w=majority
+db = client.get_database("test_db1")
+collection = db.get_collection("test_db1")
+collection.delete_many({}) 
 
 # Insert a large dataset with Write Concern w="majority" (Slower but more consistent)
 write_data(w_val="majority")
 read_data(read_level="majority")
 read_data(read_level="local")
 
+# Different collection for w=1
 db = client.get_database("test_db2")
 collection = db.get_collection("test_db2")
-collection.delete_many({})
+collection.delete_many({}) # Clear the db for testing if already populated
+
 # Insert a large dataset with Write Concern w=1 (Faster but less consistent)
 write_data(w_val=1)
 read_data(read_level="majority")
